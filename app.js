ad8ec369692f11ae3e2b3160914fe91b6c35b2eb
Logan Severin <loganseverin@utexas.edu>
2022-01-02 13:39:21 -0600

    Workshop 3

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const session = require('express-session')
+const FileStore = require('session-file-store')(session)
+const passport = require('passport')
+const authenticate = require('./authenticate')
+const config = require('./config')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const campsiteRouter = require('./routes/campsiteRouter')
+const promotionRouter = require('./routes/promotionRouter')
+const partnerRouter = require('./routes/partnerRouter')
 
-var app = express();
+const mongoose = require('mongoose')
+
+const url = config.mongoUrl
+const connect = mongoose.connect(url, {
+  useCreateIndex: true,
+  useFindAndMondify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+connect.then(() => console.log('Connected correctly to server'),
+  err => console.log(err)
+)
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -15,12 +37,29 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: false }))
+
+//app.use(cookieParser('12345-67890-09876-54321')); //should this be an environment variable before we deploy?
+
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/campsites', campsiteRouter)
+app.use('/promotions', promotionRouter)
+app.use('/partners', partnerRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
